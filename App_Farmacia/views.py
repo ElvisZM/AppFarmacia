caa59c7edf726fc5f1f0d270c75d470dbc80c1a4
Elvis Zhune <elfabri28@gmail.com>
2023-10-27 03:18:03 +0200

    muy perdido

--- App_Farmacia/views.py
+++ App_Farmacia/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Farmacia, Gerente, Empleado, Producto, Proveedor, SuministroProducto, Cliente, Compra, DetalleCompra, CuentaEmpleado, HistorialCliente, DatosFarmacia, DetalleProducto
+from django.db.models import Q
+from django.views.defaults import page_not_found
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def listar_farmacias(request):
+    farmacias = Farmacia.objects.select_related
+    
