4b482e12be8f7e88263375d088415810e711bf8e
Elvis Zhune <elfabri28@gmail.com>
2024-01-24 14:44:53 +0100

    Busqueda Simple API hecho

--- App_Farmacia/api_urls.py
+++ App_Farmacia/api_urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .api_views import *
 
 urlpatterns = [
-    path('productos', producto_list)
+    path('productos', producto_list),
+    path('productos/busqueda_simple', producto_buscar),
 ]
--- App_Farmacia/api_views.py
+++ App_Farmacia/api_views.py
@@ -2,6 +2,9 @@ from .models import *
 from .serializers import *
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
+from django.db.models import Q
+
 from .forms import *
 
 @api_view(['GET'])
@@ -11,8 +14,17 @@ def producto_list(request):
     serializer_mejorado = ProductoSerializerMejorado(productos, many=True)
     return Response(serializer_mejorado.data)
 
+@api_view(['GET'])
 def producto_buscar(request):
     if (request.user.has_perm('AppFarmacia.view_producto')):
         formulario = BusquedaProductoForm(request.query_params)
         if (formulario.is_valid()):
-            texto = formulario.cleaned_data.get('textoBusqueda')
+            texto = formulario.data.get('textoBusqueda')
+            productos = Producto.objects.select_related('farmacia_prod').prefetch_related('prov_sum_prod')
+            productos = productos.filter(Q(nombre_prod__contains=texto) | Q(descripcion__contains=texto)).all()
+            serializer = ProductoSerializerMejorado(productos, many=True)
+            return Response(serializer.data)
+        else:
+            return Response(formulario.errors, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response({"Sin permisos"}, status=status.HTTP_400_BAD_REQUEST)            
